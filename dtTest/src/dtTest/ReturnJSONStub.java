3383a4000e75eb479fce7784cedb311049269863
STester18 <STester18@ST-18.fasbaldoyle.ie>
2017-07-05 13:00:02 +0100

    working GSON and datatables implementation.

---
+++ dtTest/src/dtTest/ReturnJSONStub.java
@@ -0,0 +1,82 @@
+package dtTest;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//Uses GSON to de/serialize data
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+/**
+ * Servlet implementation class ReturnJSONStub
+ */
+@WebServlet("/ReturnJSONStub")
+public class ReturnJSONStub extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReturnJSONStub() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//sets response type to JSON
+		response.setContentType("application/json");
+		
+		//uses a List of Maps for key:value pairs
+		List<Map> data = new ArrayList<>();
+		Map<String, String> map = new HashMap<String, String>();
+		
+		//replace below loop with your ResultSet
+		for(int i=0;i<3;i++){
+			map.put("name", "Ian");
+			map.put("test", "test1");
+			map.put("number", "1");
+			data.add(map);
+		}
+		
+		String json = getJSON(data);
+		
+		//uses POJO for JSON
+		//String json = getJSON();
+		
+		//returns JSON to html
+		PrintWriter out = response.getWriter();
+		out.println(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected String getJSON(List<Map> data){
+		Gson gson = new Gson();
+		String json = gson.toJson(data);
+		
+		return json;
+	}
+	
+	protected String getJSON(){
+		SQLPOJO obj = new SQLPOJO("Ian", "test", 1);
+		Gson gson = new Gson();
+		String json = gson.toJson(obj);
+		
+		return json;
+	}
+}
---
+++ dtTest/src/dtTest/SQLPOJO.java
@@ -0,0 +1,14 @@
+package dtTest;
+
+public class SQLPOJO {
+	private String name;
+	private String test;
+	private int number;
+	
+	public SQLPOJO(String name, String test, int number) {
+		super();
+		this.name = name;
+		this.test = test;
+		this.number = number;
+	}
+}
